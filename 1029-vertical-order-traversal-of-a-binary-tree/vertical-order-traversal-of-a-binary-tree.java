005fb0d7b89001c697b74277dc6350863910538e
krajesh9 <vtu26466@veltech.edu.in>
2026-02-20 11:02:58 +0530

    Time: 4 ms (63.40%) | Memory: 44.2 MB (52.22%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.java
@@ -1,37 +1,31 @@
 class Solution {
-    void dfs( List<Node> nodes, TreeNode root, int col, int row) {
-        if(root==null) return;
-        nodes.add(new Node(root.val,col,row));
-        dfs(nodes,root.left,col-1,row+1);
-        dfs(nodes,root.right,col+1,row+1);
-    } 
-    public List<List<Integer>> verticalTraversal(TreeNode root) {
-        List<Node> nodes = new ArrayList<>();
-        dfs(nodes,root,0,0);
-        List<List<Integer>> ans = new ArrayList<>();
-        
-        nodes.sort((a,b)->{
-            if(a.col!=b.col) return Integer.compare(a.col,b.col);
-            if(a.row!=b.row) return Integer.compare(a.row,b.row);
-            return Integer.compare(a.val,b.val);
-        });
-        Integer prevCol = Integer.MIN_VALUE;
+    List<List<Integer>> list = new ArrayList<>();
 
-        for(Node cur: nodes) {
-            if(cur.col!=prevCol) {
-                prevCol = cur.col;
-                ans.add(new ArrayList<>());
-            }
-            ans.get(ans.size()-1).add(cur.val);
-        }
-        return ans;
+    public void preOrder(TreeNode root, int y, int x, Map<Integer, List<int[]>> hm){
+        if(root == null) return;
+
+        hm.computeIfAbsent(y, k -> new ArrayList<>()).add(new int[]{x, root.val});
+        preOrder(root.left, y-1, x+1, hm);
+        preOrder(root.right, y+1, x+1, hm);
     }
-    public static class Node {
-        int val, col,row;
-        Node(int v, int c, int r) {
-            val = v;
-            col = c;
-            row = r;
+
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        Map<Integer, List<int[]>> hm = new TreeMap<>();
+
+        preOrder(root, 0, 0, hm);
+
+        for(List<int[]> vals : hm.values()){
+            // sort by row (x) asc, then value asc
+            Collections.sort(vals, (a, b) -> {
+                if(a[0] != b[0]) return Integer.compare(a[0], b[0]);
+                return Integer.compare(a[1], b[1]);
+            });
+
+            List<Integer> col = new ArrayList<>();
+            for(int[] pair : vals) col.add(pair[1]);
+            list.add(col);
         }
+
+        return list;
     }
 }
