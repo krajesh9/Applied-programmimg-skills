e301879241779e47144d8ba5642e14cd3d87b014
krajesh9 <vtu26466@veltech.edu.in>
2026-04-15 11:03:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (80.51%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -1,39 +1,44 @@
-import java.util.*;
-
 class MyQueue {
-    private Deque<Integer> stackIn;
-    private Deque<Integer> stackOut;
+    Stack<Integer> s1;
+    Stack<Integer> s2;
 
     public MyQueue() {
-        stackIn = new ArrayDeque<>();
-        stackOut = new ArrayDeque<>();
+        s1 = new Stack<>();
+        s2 = new Stack<>();
     }
-
+    
     public void push(int x) {
-        stackIn.push(x);
+        s1.push(x);
     }
-
-    private void transfer() {
-        while (!stackIn.isEmpty()) {
-            stackOut.push(stackIn.pop());
-        }
-    }
-
+    
     public int pop() {
-        if (stackOut.isEmpty()) {
-            transfer();
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
         }
-        return stackOut.pop();
+        return s2.pop();
     }
-
+    
     public int peek() {
-        if (stackOut.isEmpty()) {
-            transfer();
+        if(s2.isEmpty()){
+            while(!s1.isEmpty()){
+                s2.push(s1.pop());
+            }
         }
-        return stackOut.peek();
+        return s2.peek();
     }
-
+    
     public boolean empty() {
-        return stackIn.isEmpty() && stackOut.isEmpty();
+        return s1.isEmpty() && s2.isEmpty();
     }
 }
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
