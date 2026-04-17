b8486691df0a5799c872a6951e46216a2fa3c268
krajesh9 <vtu26466@veltech.edu.in>
2026-04-17 09:32:47 +0530

    Time: 1 ms (88.69%) | Memory: 44.3 MB (43.24%) - LeetSync

--- 78-subsets/subsets.java
+++ 78-subsets/subsets.java
@@ -1,27 +1,22 @@
 class Solution {
-    List<List<Integer>> result;
     public List<List<Integer>> subsets(int[] nums) {
-        result = new ArrayList();
-        if(nums==null || nums.length==0) return result;
-        
-        subsets(nums,new ArrayList<Integer>(), 0);
-        return result;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> subset = new ArrayList<>();
+
+        createSubset(nums, 0, res, subset);
+        return res;        
     }
-    
-    private void subsets(int[] nums, ArrayList<Integer> temp, int index) {
-        // base condition
-        if(index >= nums.length) {
-            result.add(new ArrayList<>(temp));
+
+    private void createSubset(int[] nums, int index, List<List<Integer>> res, List<Integer> subset) {
+        if (index == nums.length) {
+            res.add(new ArrayList<>(subset));
             return;
         }
-        
-        // main logic
-		// case 1 : we pick the element
-        temp.add(nums[index]);
-        subsets(nums, temp, index+1); // move ahead
-        temp.remove(temp.size()-1);
-        
-		// case 2 : we don't pick the element ( notice, we did not add the current element in our temporary list
-        subsets(nums, temp, index+1); // move ahead
-    }
+
+        subset.add(nums[index]);
+        createSubset(nums, index + 1, res, subset);
+
+        subset.remove(subset.size() - 1);
+        createSubset(nums, index + 1, res, subset);
+    }    
 }
